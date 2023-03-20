380a6a27d0b3eb2a8a275e365aa48b42bd07edc9
Chantal Maskell <chanatlive@outlook.com>
2023-03-20 13:20:15 +0000

    Updated code comments on both client and server
    Updated comments to ensure code is easy to read and understand.

--- client.py
+++ client.py
@@ -4,14 +4,16 @@ import json
 import marshal
 from cryptography.fernet import Fernet
 
-
+# Client class
 class ClientSide:
+    # Initialising core variables
     def __init__(self, port, file_type=None, send_file=False, dictionary={}):
         self.dictionary = dictionary
         self.file_type = file_type
         self.send_file = send_file
 
         try:
+            # Create socket object and connect to specified port on the server
             self.s = socket.socket()
             self.host = socket.gethostname()
             self.port = port
@@ -20,6 +22,7 @@ class ClientSide:
         except socket.error as err:
             print(f'Failed to connect to socket.{err}')
 
+    # Function to manually create dictionary
     def create_dict(self):
         self.dictionary = {}
 
@@ -27,21 +30,25 @@ class ClientSide:
         key = input('Enter a key for the dictionary (press q to quit): ')
 
         # While loop to extend dictionary contents
-        # User to press 'q' to close the dictionary
+        # User to press 'q' to close the dictionary and send to the server
         while key != 'q':
             value = input('Enter a value for the key: ')
             self.dictionary[key] = value
             key = input('Enter another key for the dictionary (press q to quit): ')
 
+    # Serialise dictionary to binary format
     def serialised_binary(self):
         self.dictionary = pickle.dumps(self.dictionary)
         
+    # Serialise dictionary to JSON format
     def serialised_json(self):      
         self.dictionary = json.dumps(self.dictionary).encode()
 
+    # Serialise dictionary to XML format
     def serialised_xml(self):
         self.dictionary = marshal.dumps(self.dictionary)
 
+    # Send serialised dictionary to server
     def send_message(self):
         if len(self.dictionary) == 0:
             self.create_dict()
@@ -55,8 +62,10 @@ class ClientSide:
 
         self.s.send(self.dictionary)
 
+    # Function to send text file to server
     def send_text_file(self):
-        # Find a way to serialise text file and send through socket
+        # Prompt user to enter file location
+        # Prompt user to add encryption to the file
         file_name = input('Enter text file location: ')
         encrypt = input('Would you like to encrypt? (y/n)')
        
@@ -67,22 +76,27 @@ class ClientSide:
             self.serialised_data = pickle.dumps(file_contents)
 
         else:
-            # Generate a random key
+            # Generate a random key and encrypt the file contents
             key = Fernet.generate_key()
 
             # Initialize the Fernet object with the key
             fernet = Fernet(key)
 
+            # Send the serialised data to the server
             self.serialised_data = pickle.dumps(fernet.encrypt(file_contents))
 
         self.s.send(self.serialised_data)
 
+    # Function to run the overall client
+    # and send either a dictionary or text file to server
     def run_client(self):
         if self.send_file == True:
             self.send_text_file()
         else:
             self.send_message()
 
-
+# Create a client object with port and the option to send a file
 client = ClientSide(56789, send_file=True)
+
+# Run the client
 client.run_client()
--- server.py
+++ server.py
@@ -4,24 +4,30 @@ import json
 import marshal
 import magic
 
+# Server class
 class ServerSide:
+    # Initialising core variables
     def __init__(self, port, save_file=False):
         self.s = socket.socket()
         self.host = socket.gethostname()
         self.port = port
 
     def server_check(self):
+        # Binding the socket to the host and port
         self.s.bind((self.host, self.port))
 
+        # Listen for incoming connections
         try:
             self.s.listen(5)
             print('Server is listening...')
+        # Raise exception if connection is not received
         except:
             raise Exception('Server failed!')
 
     def test_connection(self):
         while True:
             try:
+                # Accept incoming connections from client
                 self.conn, addr = self.s.accept()
                 if addr is None:
                     raise Exception('Problem with address.')
@@ -32,21 +38,26 @@ class ServerSide:
                 raise Exception('Connection error.')
 
     def deserialise_dict(self):
+        # Receive data from the client
         received_dict = self.conn.recv(4096)
+
+        # Use magic to determine the file type of the received data
         m = magic.Magic(mime=True)
         file_type = m.from_buffer(received_dict)
         file_saved = False
 
-        if file_type == 'application/octet-stream':
         # Check if the contents of the dictionary match the format of binary data
+        if file_type == 'application/octet-stream':
             try:
+                # Depickling binary data
                 self.loaded_dict = pickle.loads(received_dict)
-                print('Binary file type received. Now depickling.')
+                print('Binary file type received. Now depickling...')
                 
                 # Checks if recieved dict is bytes object
                 if isinstance(self.loaded_dict, bytes):
                     file_saved = True
                     with open("C:\\Users\chana\\Documents\\GitHub\\client_server_network\\server_cache\\text.txt", 'wb') as f:
+                        # Saving received bytes object as text file
                         f.write(self.loaded_dict)
 
                     print('Text File Saved.')
@@ -54,6 +65,7 @@ class ServerSide:
                 print('XML file type received. Now loading.')
                 self.loaded_dict = marshal.loads(received_dict)
 
+        # Check if the contents of the dictionary match the format of JSON data
         elif file_type == 'text/plain':
         # Check if the contents of the dictionary match the format of JSON data
             try:
@@ -63,6 +75,7 @@ class ServerSide:
                 raise Exception('Invalid JSON file.')
             
         else:
+            # Unsupported file type received
             print(f'File type: {file_type} not supported')
 
         if file_saved == False:
